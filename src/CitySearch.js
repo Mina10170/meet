9744d0f0dc09484d714b631a32732079a92b947a
Mina10170 <89662271+Mina10170@users.noreply.github.com>
2021-12-17 21:36:13 +0100

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -11,7 +11,7 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({showSuggestions:true});
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
@@ -24,10 +24,10 @@ class CitySearch extends Component {
       return this.setState({
         query: value,
         suggestions,
-        infoText:''
+        infoText: ''
       });
     }
-  };
+  }
 
   handleItemClicked = (suggestion) => {
     this.setState({
@@ -38,27 +38,32 @@ class CitySearch extends Component {
   }
 
   render() {
+    const { numberOfEvents } = this.props;
+
     return (
-      <div className="CitySearch">
-         <InfoAlert text={this.state.infoText} />
-        <input
-  type="text"
-  className="city"
-  value={this.state.query}
-  onChange={this.handleInputChanged}
-  onFocus={() => { this.setState({ showSuggestions: true }) }}
-/>
-    <ul className="suggestions">
-  {this.state.suggestions.map((suggestion) => (
-    <li
-      key={suggestion}
-      onClick={() => this.handleItemClicked(suggestion)}
-    >{suggestion}</li>
-  ))}
- <li onClick={() => this.handleItemClicked("all")}>
-  <b>See all cities</b>
-</li>
-</ul>
+      <div className="citySearch">
+        <div className="citySearch__alert">
+          <InfoAlert text={this.state.infoText} />
+        </div>
+        
+        <div className="citySearch__grid">
+          <label htmlFor={this.state.query}>Search for a city</label>
+          <input type="text"
+            className="city"
+            value={this.state.query}
+            onChange={this.handleInputChanged}
+            onFocus={() => { this.setState({ showSuggestions: true }) }}
+          />
+
+          <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
+            {this.state.suggestions.map((suggestion) => (
+              <li key={suggestion} onClick={() => this.handleSuggestionClicked(suggestion, numberOfEvents)}>{suggestion}</li>
+            ))}
+            <li key="all" onClick={() => this.handleSuggestionClicked("all", numberOfEvents)}>
+              <b >See all cities</b>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
